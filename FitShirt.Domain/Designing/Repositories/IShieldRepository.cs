fc608c026df3fbb663500f8c67b83cb217b2d011
Diego Defilippi <defsan35@gmail.com>
2024-06-14 22:31:50 -0500

    feat: added designing repositories

---
+++ FitShirt.Domain/Designing/Repositories/IShieldRepository.cs
@@ -0,0 +1,9 @@
+using FitShirt.Domain.Designing.Models.Entities;
+using FitShirt.Domain.Shared.Repositories;
+
+namespace FitShirt.Domain.Designing.Repositories;
+
+public interface IShieldRepository : IBaseRepository<Shield>
+{
+    
+}
